06f7baa1359681a7ba3439e28b7118d3b6719cf7
ZikXewen <datasup12@gmail.com>
2025-09-01 00:57:29 -0500

    setup

--- src/main/kotlin/zikxewen/productive_cats/Main.kt
+++ src/main/kotlin/zikxewen/productive_cats/Main.kt
@@ -1,7 +1,6 @@
 package zikxewen.test_neoforge_kt
 
 import net.neoforged.fml.common.Mod
-import net.neoforged.neoforge.registries.DeferredRegister
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -9,14 +8,11 @@ import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.neoforge.forge.getValue
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
-@Mod(TestMod.ID)
-object TestMod {
-  const val ID = "test_neoforge_kt"
-  val ITEM_REGISTRY = DeferredRegister.createItems(ID)
-  val TEST_ITEM by ITEM_REGISTRY.registerSimpleItem("test_item")
+@Mod(Main.ID)
+object Main {
+  const val ID = "productive_cats"
   val LOGGER: Logger = LogManager.getLogger(ID)
   init {
     LOGGER.log(Level.INFO, "Hello, World!")
-    ITEM_REGISTRY.register(MOD_BUS)
   }
 }
